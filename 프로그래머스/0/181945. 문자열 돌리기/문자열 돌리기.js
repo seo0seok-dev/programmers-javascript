f575795968ba9bdb8445464bd6fe3b8001f70c74
seo0seok-dev <seo0seok.dev@gmail.com>
2026-03-18 11:20:07 +0900

    [level 0] Title: 문자열 돌리기, Time: 299.12 ms, Memory: 31.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    for (i of input[0]){
+        console.log(i);
+    }
+});
