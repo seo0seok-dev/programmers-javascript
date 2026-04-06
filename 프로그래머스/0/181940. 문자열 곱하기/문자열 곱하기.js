4956b2f495b819543fca53a010de6a6fca84cc5d
seo0seok-dev <seo0seok.dev@gmail.com>
2026-04-06 15:29:05 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,5 +1,24 @@
+// repeat
 function solution(my_string, k) {
-    var answer = Array(k).fill(my_string).join("");
+    var answer = my_string.repeat(k);
     
-    return answer;
+    return answer
 }
+
+// 반복문
+// function solution(my_string, k) {
+//     var answer = '';
+//     for(i=0;i<k;i++){
+//         answer += my_string;
+//     }
+//     var answer = Array(k).fill(my_string).join("");
+    
+//     return answer;
+// }
+
+// 배열
+// function solution(my_string, k) {
+//     var answer = Array(k).fill(my_string).join("");
+    
+//     return answer;
+// }
