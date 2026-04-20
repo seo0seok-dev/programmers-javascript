1c3ca6099e07158c82706fef9631654c42c62a84
seo0seok-dev <seo0seok.dev@gmail.com>
2026-04-20 15:38:14 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = 0;
+    if (n > 7){
+        if (n % 7 === 0){
+            answer = Math.floor(n / 7);    
+        } else {
+            answer = Math.floor(n / 7) + 1;
+        }
+        
+    } else {
+        answer = 1;
+    }
+    return answer;
+}
