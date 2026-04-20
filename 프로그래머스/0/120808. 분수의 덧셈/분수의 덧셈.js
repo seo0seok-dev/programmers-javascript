abbbef83352d3a6112dc952792668869bff7e0fd
seo0seok-dev <seo0seok.dev@gmail.com>
2026-04-20 15:04:32 +0900

    [level 0] Title: 분수의 덧셈, Time: 9.94 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,15 @@
+function solution(numer1, denom1, numer2, denom2) {    
+    let numer = (numer1 * denom2) + (numer2 * denom1);
+    let denom = denom1 * denom2;
+  
+    let min = Math.min(numer, denom)
+    let gcd = 1;
+    
+    for(let i=1; i <= min; i++){
+        if(numer % i === 0 && denom % i === 0){
+            gcd = i;
+        }
+    }
+        
+    return [ numer / gcd, denom / gcd ];
+}
